c65a1f0be323b2027f157b73ee6a2c791ed59d07
Ілля Чуєв Ка-17 <chuev120@gmail.com>
2022-11-01 17:09:49 +0300

    Added projects

---
+++ Back-end/AutoRia-UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoRia_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
