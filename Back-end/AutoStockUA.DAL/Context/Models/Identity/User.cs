a4298b028d415ffccd08ba51e8daa05a9213f7cc
ChuevIluka <chuev120@gmail.com>
2022-11-10 20:47:24 +0200

    Deleted repos except generic, expressions added to generic

--- Back-end/AutoStockUA.DAL/Context/Models/Identity/User.cs
+++ Back-end/AutoStockUA.DAL/Context/Models/Identity/User.cs
@@ -15,6 +15,7 @@ namespace AutoStockUA.DAL.Context.Models.Identity
         public virtual List<Message> Messages { get; set; }
         public virtual List<Comment> Comments { get; set; }
         public List<Chat> Chats { get; set; }
+        public byte[] Avatar { get; set; }
         public List<Advertisement> Advertisements { get; set; }
         public IList<Favourites> Favourites { get; set; }
     }
