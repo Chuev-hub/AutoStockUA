9563468f84f769f492cbb0593399c6091f945e55
ChuevIluka <chuev120@gmail.com>
2022-11-10 23:12:48 +0200

    another services added

---
+++ Back-end/AutoStockUA.BLL/Services/AdvertisementService.cs
@@ -0,0 +1,21 @@
+﻿using AutoStockUA.BLL.DTO.Ad;
+using AutoStockUA.DAL.Context;
+using AutoStockUA.DAL.Context.Models.Ad;
+using AutoStockUA.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoStockUA.BLL.Services
+{
+    public class AdvertisementService : GenericService<AdvertisementDTO, Advertisement>
+    {
+        public AdvertisementService(AutoStockContext context):base(context)
+        {
+
+        }
+    }
+}
